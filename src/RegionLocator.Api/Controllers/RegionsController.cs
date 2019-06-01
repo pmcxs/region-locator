0b40a660487f9bd55d9f4498418df1ced2315f2b
pedro.sousa <pedro.sousa@sky.uk>
2019-06-02 00:41:42 +0100

    First working version

---
+++ src/RegionLocator.Api/Controllers/RegionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegionLocator.Core;
+
+namespace RegionLocator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly IRegionsLookupHandler _handler;
+
+        public RegionsController(IRegionsLookupHandler handler)
+        {
+            this._handler = handler;
+        }
+
+        // GET api/Regions/byCoordinates
+        [HttpGet("byCoordinates")]
+        public ActionResult<Region> Get(string longitude, string latitude)
+        {
+            double lon = double.Parse(longitude);
+            double lat = double.Parse(latitude);
+
+            Region dma = _handler.GetRegion(lon, lat);
+
+            if(dma == null)
+            {
+                return NotFound();
+            }
+
+            return dma;
+        }
+    }
+}
---
+++ src/RegionLocator.Api/ShapefileRegionsLookupHandler.cs
@@ -0,0 +1,110 @@
+﻿using System.IO;
+using System.Linq;
+using GeoAPI.Geometries;
+using Microsoft.Extensions.Configuration;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using RegionLocator.Core;
+
+namespace RegionLocator.API
+{
+    public class ShapefileRegionsLookupHandler : IRegionsLookupHandler
+    {
+        private readonly FeatureCollection _features;
+
+        public ShapefileRegionsLookupHandler(IConfiguration configuration)
+        {
+            string[] propertiesToInclude = configuration
+                .GetSection("shapefileProperties")
+                .GetChildren()
+                .ToList()
+                .Select(c => c.Value.ToUpper())
+                .ToArray();
+
+            _features = LoadFeatures(configuration["shapefilePath"], propertiesToInclude);
+
+        }
+
+        public Region GetRegion(double lon, double lat)
+        {
+            return QueryRegions(new Coordinate(lon, lat));
+        }
+
+        private FeatureCollection LoadFeatures(string shpFilename, string[] propertiesToInclude)
+        {
+            FeatureCollection features = new FeatureCollection();
+
+            GeometryFactory factory = new GeometryFactory();
+
+            using (var shapeFileDataReader = new ShapefileDataReader(shpFilename, factory))
+            {
+                DbaseFileHeader header = shapeFileDataReader.DbaseHeader;
+
+                while (shapeFileDataReader.Read())
+                {
+                    Feature feature = new Feature();
+                    AttributesTable attributesTable = new AttributesTable();
+
+                    string[] keys = new string[header.NumFields];
+
+                    IGeometry geometry = (Geometry)shapeFileDataReader.Geometry;
+
+                    for (int i = 0; i < header.NumFields; i++)
+                    {
+                        DbaseFieldDescriptor fldDescriptor = header.Fields[i];
+
+                        if(propertiesToInclude.Contains(fldDescriptor.Name.ToUpper()))
+                        {
+                            keys[i] = fldDescriptor.Name;
+                            attributesTable.Add(fldDescriptor.Name, shapeFileDataReader.GetValue(i + 1));
+                        }
+                    }
+
+                    feature.Geometry = geometry;
+                    IGeometry envelope = geometry.Envelope;
+                    feature.BoundingBox = new Envelope(envelope.Coordinates[0], envelope.Coordinates[2]);
+                    feature.Attributes = attributesTable;
+                    features.Add(feature);
+                }
+            }
+
+            return features;
+        }
+
+        private Region QueryRegions(Coordinate coordinate)
+        {
+            IFeature matchFeature = null;
+
+            for (var i = 0; i < _features.Count; i++)
+            {
+                if (_features[i].BoundingBox.Contains(coordinate))
+                {
+                    if (_features[i].Geometry.Contains(new Point(coordinate)))
+                    {
+                        matchFeature = _features[i];
+                        break;
+                    }
+                }
+            }
+
+            if (matchFeature == null)
+                return null;
+
+            var dma = new Region
+            {
+                Properties = new System.Collections.Generic.Dictionary<string, object>()
+            };
+
+
+            foreach (var attributeName in matchFeature.Attributes.GetNames())
+            {
+                dma.Properties.Add(attributeName, matchFeature.Attributes[attributeName]);
+
+            }
+
+            return dma;
+
+        }
+    }
+}
